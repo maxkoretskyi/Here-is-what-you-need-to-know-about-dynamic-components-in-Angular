4127cbdac5459fe66c957d29444a2840bb33474b
maksym.koretskyi <maksym_koretskyi_tp@bmc.com>
2017-04-28 13:22:21 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+Angular seed
+============
+This is basic Angular setup with the minimum required dependencies
+
+Running the app
+---------------
+
+```
+npm i
+npm start
+```
---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
+})
+export class AppComponent {
+  name = 'Angular';
+}
